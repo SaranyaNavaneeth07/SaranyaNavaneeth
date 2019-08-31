be6ceb38fa8fe2cf15d796a7995f32457cfaa9ac
SaranyaNavaneeth07 <saranyanavaneeth07@gmail.com>
2019-08-31 22:58:20 +0530

    Core Java

---
+++ src/org/cts/testing/SumOfOdd.java
@@ -0,0 +1,20 @@
+package org.cts.testing;
+
+public class SumOfOdd {
+	public static void main(String[] args) {
+		int Count = 0;
+		
+	for (int i = 0; i <=100 ; i++){
+		if (i % 2 ==1) {
+		Count = Count + i;	
+			
+		}
+	
+	}
+	System.out.println(Count);
+			
+		}
+		
+	}
+
+
